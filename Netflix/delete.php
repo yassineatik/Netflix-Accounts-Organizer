23d963aebfa6ddc1e988b3a5f5409957963a96a3
yassineatik <77692478+yassineatik@users.noreply.github.com>
2022-10-20 23:51:31 +0100

    Delete Netflix directory

--- Netflix/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-require 'includes/db.php';
-$conn->exec("DELETE FROM accounts");
-header('Location: index.php');
--- Netflix/includes/errors.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$errors = array(
-    '1' => "Please Attach Text File(.txt)",
-    '2' => "File Size is too big",
-    '3' => "Attached File Is Empty"
-);
